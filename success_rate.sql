b3415ebf71f35958e0aa838cb576a5dd04558ba1
IoannMartynov <162453077+IoannMartynov@users.noreply.github.com>
2024-06-23 11:34:45 +0300

    Update success_rate.sql

--- success_rate.sql
+++ success_rate.sql
@@ -1,11 +1,11 @@
 select
     date_trunc('month', date) :: date as time,
-    mode,
+    card_type,
     (cast(sum(case when status = 'Success' then 1 else 0 end) as float) / count(*)) * 100 as success_rate
 from
     new_payments
 where
-    mode != 'Не определено'
+    mode != 'Undefined'
 group by
     date_trunc('month', date),
     mode
